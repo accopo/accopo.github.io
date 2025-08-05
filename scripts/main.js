dc549607223a0faf0303a6b49344b7d67f515a7a
accopo <xiaoguoyuyu050910@gmail.com>
2025-08-05 15:27:28 +0800

    版本1.0更新

--- scripts/main.js
+++ scripts/main.js
@@ -15,14 +15,14 @@ let myHeading = document.querySelector("h1");
 function setUserName() {
   const myName = prompt("Please enter your name.");
   localStorage.setItem("name", myName);
-  myHeading.textContent = `Mozilla is cool, ${myName}`;
+  myHeading.textContent = `欢迎来到我的网站！, ${myName}`;
 }
 
 if (!localStorage.getItem("name")) {
   setUserName();
 } else {
   const storedName = localStorage.getItem("name");
-  myHeading.textContent = `Mozilla is cool, ${storedName}`;
+  myHeading.textContent = `欢迎来到我的网站！, ${storedName}`;
 }
 
 myButton.addEventListener("click", () => {
@@ -35,6 +35,6 @@ function setUserName() {
     setUserName();
   } else {
     localStorage.setItem("name", myName);
-    myHeading.textContent = `Mozilla is cool, ${myName}`;
+    myHeading.textContent = `欢迎来到我的网站！, ${myName}`;
   }
 }
